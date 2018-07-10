68b6e7fbf8d3df646bfd3815e7991b042388f910
Thomas Pham <Tom@localhost.localdomain>
2018-07-09 17:16:14 -0700

    add weekday script

---
+++ weekday.js
@@ -0,0 +1,6 @@
+function dayName(date) {   
+     const daysOfTheWeek = ["Sunday", "Monday", "Tuesday",
+     "wednesday", "Thursday", "Friday", "Saturday"];
+
+    return daysOfTheWeek[date.getDay()];
+}
